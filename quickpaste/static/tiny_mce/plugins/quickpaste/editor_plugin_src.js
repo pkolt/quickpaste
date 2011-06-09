de12741130a338043d25c24c6023b202e3d8dac8
pkolt <pkolt@mail.ru>
2011-06-09 17:52:48 +0600

    Integration with Django CMS. Migration to the Generic Based Views

---
+++ quickpaste/static/tiny_mce/plugins/quickpaste/editor_plugin_src.js
@@ -0,0 +1,41 @@
+(function() {
+	tinymce.PluginManager.requireLangPack('quickpaste');
+	tinymce.create('tinymce.plugins.QuickPastePlugin', {
+		init : function(ed, url) {
+			ed.addCommand('mceDownloader', function() {
+				ed.windowManager.open({
+					file : '/quickpaste/',
+					width : 420 + parseInt(ed.getLang('quickpaste.delta_width', 0)),
+					height : 140 + parseInt(ed.getLang('quickpaste.delta_height', 0)),
+					inline : 1
+				}, {
+					plugin_url : url,
+					some_custom_arg : 'custom arg'
+				});
+			});
+
+			ed.addButton('quickpaste', {
+				title : 'quickpaste.desc',
+				cmd : 'mceDownloader',
+				image : url + '/img/quickpaste.gif'
+			});
+
+		},
+
+		createControl : function(n, cm) {
+			return null;
+		},
+
+		getInfo : function() {
+			return {
+				longname : 'quickpaste plugin',
+				author : 'Pavel Koltyshev',
+				authorurl : 'http://github.com/pkolt',
+				infourl : 'http://github.com/pkolt',
+				version : "1.0"
+			};
+		}
+	});
+
+	tinymce.PluginManager.add('quickpaste', tinymce.plugins.QuickPastePlugin);
+})();
---
+++ quickpaste/static/tiny_mce/plugins/quickpaste/langs/en.js
@@ -0,0 +1,3 @@
+tinyMCE.addI18n('en.quickpaste',{
+	desc : 'Add file'
+});
